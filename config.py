85d84972abdb561095c3e03f5868bc4c1a7d528f
Ethosa <social.ethosa@gmail.com>
2022-10-21 18:53:47 +0700

    update config

--- config.py
+++ config.py
@@ -3,10 +3,13 @@ GROUP_ID = 0
 
 # data for dm images
 DM_DATA = [
-    ("title", "subtitle"),  # title and subtitle
-    "title",  # only title
+    # ("title", "subtitle"),  # title and subtitle
+    # "title",  # only title
 ]
 
 VOTE_TIMEOUT = 60 * 30  # 30 minutes
-
-ADMINS = [556962840]
+ADMINS = []
+MESSAGE_STATE = 25  # After 25 messages sends random message
+MESSAGE_STATES = [
+    # "message text"
+]
